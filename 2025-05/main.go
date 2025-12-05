6e80ed201e655f3ca56f615a410afe7d748e04a9
kh <kylemhouseholder@gmail.com>
2025-12-05 09:16:59 -0600

    cleaned up comments

--- 2025-05/main.go
+++ 2025-05/main.go
@@ -58,24 +58,24 @@ func evalPart2(ranges []string) int {
 	sort.Slice(ingrRanges, func(i, j int) bool {
 		return ingrRanges[i][0] < ingrRanges[j][0]
 	})
-	fmt.Printf("ranges: %v\n", ingrRanges)
+	// fmt.Printf("ranges: %v\n", ingrRanges)
 
 	for _, ingrRange := range ingrRanges {
-		fmt.Printf("Evaluating %v", ingrRange)
+		// fmt.Printf("Evaluating %v", ingrRange)
 		if ingrRange[1] < maxValueSeen {
-			fmt.Printf(" skip, upper bound less than max\n")
+			// fmt.Printf(" skip, upper bound less than max\n")
 			continue
 		}
-		fmt.Printf("\n")
+		// fmt.Printf("\n")
 
 		upper := ingrRange[1]
 		lower := int(math.Max(float64(maxValueSeen)+1, float64(ingrRange[0])))
 		maxValueSeen = ingrRange[1]
-		fmt.Printf("\tsetting lower to: %d, maxValueSeen to: %d\n", lower, maxValueSeen)
+		// fmt.Printf("\tsetting lower to: %d, maxValueSeen to: %d\n", lower, maxValueSeen)
 
 		diff := upper - lower + 1
 		count += diff
-		fmt.Printf("\tfound %d ingredients to add to count\n", diff)
+		// fmt.Printf("\tfound %d ingredients to add to count\n", diff)
 	}
 
 	return count
